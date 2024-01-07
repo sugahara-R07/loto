bb9abec45536dceff509c36064af41f514052261
SR-07 <dc.s7.charlotte@gmail.com>
2024-01-08 00:04:16 +0900

    初回

---
+++ Loto/Loto7_Script.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loto
+{
+    internal class Loto7_Script
+    {
+        public List<int> ans = new List<int>();
+
+        Random rnd = new Random();
+
+        public void loto7Ans(int loto , int max)
+        {
+            ans.Clear();
+
+           while (ans.Count < loto) 
+           {
+                if (ans.Count == loto)
+                    return;
+                
+                int num = rnd.Next(1,max);
+                bool flag = false;
+                
+                for(int i= 0; i< ans.Count;i++)
+                {
+                    if (ans[i] != num)
+                    {
+                        continue;
+                    }
+
+                    flag = true;
+                }
+
+                if (flag)
+                    continue;
+
+                ans.Add(num);
+
+           }
+
+            Console.WriteLine(ans.Count);
+        }
+
+        public void AnsWrite()
+        {
+            ans.Sort();
+            for (int i = 0; i < ans.Count; i++)
+            {
+                Console.Write(ans[i] + ",");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Loto/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Loto
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Loto7_Script _loto7 = new Loto7_Script();
+           
+            while (true)
+            {
+                _loto7.loto7Ans(6,43);
+                _loto7.AnsWrite();
+
+                Console.Write("やめるなら[1]を続けるならそれ以外を入力：");
+                string s = Console.ReadLine();
+                Console.WriteLine();
+                
+                if(s == "1")
+                {
+                    break;
+                }
+
+            }
+        }
+    }
+}
